d7ccbeb99aa3a1d8085600299c5cceb660e7d009
negaonka <neha.n.gaonkar@gmail.com>
2022-06-24 13:15:05 +0100

    correcting code configs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -68,11 +68,3 @@ export const QUERY_USER = gql`
     }
   }
 `;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://negaonka:neha131511@cluster0.9e4kb.mongodb.net/mernshopping?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
